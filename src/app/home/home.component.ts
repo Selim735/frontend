38069cac754974134b546012e2b919802928815a
selim <s.benslimen2706@myu.imset.tn>
2025-01-21 00:41:32 +0100

    create page Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface NavItem {
+  label: string;
+  route: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-home',
   standalone: false,
@@ -8,5 +14,16 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  isMobileMenuOpen = false;
+  
+  navItems: NavItem[] = [
+    { label: 'Home', route: '/', icon: 'fas fa-home' },
+    { label: 'Schedule', route: '/schedule', icon: 'fas fa-calendar-alt' },
+    { label: 'Track Bus', route: '/track', icon: 'fas fa-bus' },
+    { label: 'Stations', route: '/stations', icon: 'fas fa-map-marker-alt' }
+  ];
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
