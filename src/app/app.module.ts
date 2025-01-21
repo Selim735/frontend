9cb20aed19b2e2afc71aca006e26428efb268fc1
SarraGobjii <sarra.gobji@sesame.com.tn>
2025-01-21 13:21:43 +0100

    content change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
-// app.module.ts
-
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';  // <-- import this
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -20,10 +18,11 @@ import { AdminComponent } from './admin/admin.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,  // <-- add this
+    HttpClientModule,
+
   ],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
